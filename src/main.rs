bad47a2be5a233da48731674f69d414bae331167
Zan <69921455+InZan17@users.noreply.github.com>
2024-09-16 13:49:13 +0200

    Change usage of smooth delta

--- src/main.rs
+++ src/main.rs
@@ -37,6 +37,7 @@ async fn main() {
     let mut delta_window_position = Vec2::ZERO;
 
     let mut smoothed_delta = Vec2::ZERO;
+    let mut smoothed_magnitude = 0.;
 
     let gravity_strength = 3000.;
     let air_friction = 0.1;
@@ -46,28 +47,40 @@ async fn main() {
     let mut ball_position = Vec2::ZERO;
     let mut ball_velocity = Vec2::ZERO;
 
-
     loop {
         if is_mouse_button_down(MouseButton::Left) {
-            delta_window_position = mouse_delta_position() * Vec2::new(WIDTH_F / 2., HEIGHT_F / 2.) + delta_window_position;
+            delta_window_position = mouse_delta_position() * Vec2::new(WIDTH_F / 2., HEIGHT_F / 2.)
+                + delta_window_position;
             last_window_position -= delta_window_position;
             set_window_position(last_window_position.x as u32, last_window_position.y as u32);
         } else {
-            let current_window_position = Vec2::from_u32_tuple(miniquad::window::get_window_position());
+            let current_window_position =
+                Vec2::from_u32_tuple(miniquad::window::get_window_position());
             delta_window_position = last_window_position - current_window_position;
             last_window_position = current_window_position;
         }
-        
-        smoothed_delta = smoothed_delta.lerp(delta_window_position, 0.5);
+
+        smoothed_delta = smoothed_delta.lerp(delta_window_position, 0.25);
+        smoothed_magnitude = smoothed_magnitude
+            .lerp(smoothed_delta.length(), 0.15)
+            .min(smoothed_delta.length());
+
+        let smoothed_delta = if smoothed_delta.length() != 0. {
+            smoothed_delta.normalize() * smoothed_magnitude
+        } else {
+            smoothed_delta
+        };
+
+        let maxed_delta = smoothed_delta.max(delta_window_position) / get_frame_time() * 2.;
 
         clear_background(LIGHTGRAY);
 
         ball_velocity += Vec2::new(0., gravity_strength * get_frame_time());
 
-        ball_velocity *= 1.-(air_friction * get_frame_time().clamp(0., 1.));
+        ball_velocity *= 1. - (air_friction * get_frame_time().clamp(0., 1.));
 
         let total_velocity = if time::get_time() > 1. {
-            ball_velocity + (smoothed_delta / get_frame_time()) * 2.
+            ball_velocity + (delta_window_position / get_frame_time()) * 2.
         } else {
             ball_velocity
         };
@@ -81,23 +94,19 @@ async fn main() {
 
         if ball_position.y > HEIGHT_F - radius {
             ball_position.y = HEIGHT_F - radius;
-            ball_velocity.y = -total_velocity.y * bounciness;
-            println!("{ball_velocity}");
-            println!("Floor");
+            ball_velocity.y = -(ball_velocity.y + maxed_delta.y) * bounciness;
         } else if ball_position.y < -HEIGHT_F + radius {
             ball_position.y = -HEIGHT_F + radius;
-            ball_velocity.y = -total_velocity.y * bounciness;
-            println!("Ceil");
+            ball_velocity.y = -(ball_velocity.y + maxed_delta.y) * bounciness;
         }
 
         if ball_position.x > WIDTH_F - radius {
             ball_position.x = WIDTH_F - radius;
-            ball_velocity.x = -total_velocity.x * bounciness;
+            ball_velocity.x = -(ball_velocity.x + maxed_delta.x) * bounciness;
         } else if ball_position.x < -WIDTH_F + radius {
             ball_position.x = -WIDTH_F + radius;
-            ball_velocity.x = -total_velocity.x * bounciness;
+            ball_velocity.x = -(ball_velocity.x + maxed_delta.x) * bounciness;
         }
-
 
         if ball_velocity.length() > terminal_velocity {
             println!("Reached terminal velocity!");

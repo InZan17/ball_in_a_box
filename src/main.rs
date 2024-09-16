ce6c0a375faab8493a585d785efcfa8f4f58efe4
Zan <69921455+InZan17@users.noreply.github.com>
2024-09-16 13:07:14 +0200

    Add bounces, velocity and mouse grabbing window

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use macroquad::{prelude::*, time};
 use miniquad::*;
+use window::set_window_position;
 
 pub fn window_conf() -> miniquad::conf::Conf {
     miniquad::conf::Conf {
@@ -27,28 +28,57 @@ impl FromTuple for Vec2 {
 #[macroquad::main(window_conf)]
 async fn main() {
     let mut last_window_position = Vec2::from_u32_tuple(miniquad::window::get_window_position());
+    let mut delta_window_position = Vec2::ZERO;
 
-    let mut ball_position = Vec2::new(0., 0.);
+    let gravity_strength = 1000.;
+    let bounciness = 0.9;
+    let mut ball_position = Vec2::ZERO;
+    let mut ball_velocity = Vec2::ZERO;
+    let terminal_velocity = 8000.;
+
 
     loop {
-        let current_window_position = Vec2::from_u32_tuple(miniquad::window::get_window_position());
-        let delta_position = last_window_position - current_window_position;
-        last_window_position = current_window_position;
+        if is_mouse_button_down(MouseButton::Left) {
+            delta_window_position = mouse_delta_position() * Vec2::new(400., 300.) + delta_window_position;
+            last_window_position -= delta_window_position;
+            set_window_position(last_window_position.x as u32, last_window_position.y as u32);
+        } else {
+            let current_window_position = Vec2::from_u32_tuple(miniquad::window::get_window_position());
+            delta_window_position = last_window_position - current_window_position;
+            last_window_position = current_window_position;
+        }
 
-        println!("{:?}", delta_position);
         clear_background(LIGHTGRAY);
 
-        let ball_velocity = delta_position;
+        ball_velocity += Vec2::new(0., gravity_strength * get_frame_time());
 
-        if time::get_time() > 1. {
-            ball_position += ball_velocity * 2.;
-        }
+        let total_velocity = if time::get_time() > 1. {
+            ball_velocity + (delta_window_position / get_frame_time()) * 2.
+        } else {
+            ball_velocity
+        };
+
+        ball_position += total_velocity * get_frame_time();
 
         set_camera(&Camera2D {
             zoom: vec2(1. / 800., 1. / 600.),
             ..Default::default()
         });
-        draw_circle(ball_position.x, ball_position.y, 40., YELLOW);
+
+        if ball_position.y > 600. {
+            ball_position.y = 600.;
+            ball_velocity.y = -total_velocity.y * bounciness;
+        } else if ball_position.y < -600. {
+            ball_position.y = -600.;
+            ball_velocity.y = -total_velocity.y * bounciness;
+        }
+
+
+        if ball_velocity.length() > terminal_velocity {
+            ball_velocity = ball_velocity.normalize() * terminal_velocity;
+        }
+
+        draw_circle(ball_position.x, ball_position.y, 40., BLUE);
 
         next_frame().await
     }
